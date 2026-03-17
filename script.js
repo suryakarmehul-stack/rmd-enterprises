06a1ca8d0a830b6bd973c626feca025f977e2dd3
suryakarmehul-stack <suryakarmehul@gmail.com>
2026-03-17 21:38:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("RMD Website Loaded");
+function emi(){
+let p=price.value;
+document.getElementById("emiResult").innerText="अंदाजे EMI ₹"+(p/12);
+}
+function send(){
+window.open("https://wa.me/917588031941?text=Loan Request");
+}
